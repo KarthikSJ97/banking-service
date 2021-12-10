a831c2bc96d7227ee81276a5cecfc4abe7d1c131
KarthikSJ97 <karthik.s.jhingade@gmail.com>
2021-12-10 18:51:11 +0530

    Karthik | Initial setup application

---
+++ src/main/java/com/demo/banking_service_app/BankingServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.banking_service_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingServiceAppApplication.class, args);
+	}
+
+}
